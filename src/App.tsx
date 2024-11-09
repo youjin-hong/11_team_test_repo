491d00f7f18c9aaf665acf2bd939264815bf9d3c
asdfghjk1052@naver.com <asdfghjk1052@naver.com>
2024-11-09 18:39:11 +0900

    feat: 번역 기능, styled-components 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+
+const IntroTitle = styled.p`
+  font-size: 1.5rem;
+  color: #bf4f74;
+`;
+
+const TranslateButton = styled.button`
+  background-color: antiquewhite;
+  border-radius: 1rem;
+  padding: 1rem;
+  margin: 5px;
+  border: none;
+  cursor: pointer;
+`;
+
 function App() {
-  return <>번역기능 구현해보기</>;
+  const { t, i18n } = useTranslation();
+
+  // 언어 전환 함수
+  const changeLanguage = (language: string) => {
+    i18n.changeLanguage(language);
+  };
+
+  return (
+    <div>
+      <IntroTitle>{t('hello')}</IntroTitle>
+      <TranslateButton onClick={() => changeLanguage('en-US')}>English</TranslateButton>
+      <TranslateButton onClick={() => changeLanguage('ko-KR')}>한국어</TranslateButton>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/i18n.ts
@@ -0,0 +1,38 @@
+// import i18n from 'i18next';
+// import ko_KR from './assets/locales/ko-KR/translation.json';
+// import en_US from './assets/locales/en-US/translation.json';
+// import { initReactI18next } from 'react-i18next';
+
+// const resources = {
+//   'ko-KR': { translation: ko_KR },
+//   'en-US': { translation: en_US },
+// };
+
+// i18n.use(initReactI18next).init({
+//   resources,
+//   fallbackLng: 'en', // 기본 언어 설정
+// });
+
+// export default i18n;
+
+// i18n.ts
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import koKR from './assets/locales/ko-KR/translation.json';
+import enUS from './assets/locales/en-US/translation.json';
+
+// 번역 리소스를 설정
+const resources = {
+  'ko-KR': { translation: koKR },
+  'en-US': { translation: enUS },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  fallbackLng: 'ko-KR', // 초기 언어는 한국어로 설정
+  interpolation: {
+    escapeValue: false, // React에서는 기본적으로 XSS 방지 처리가 되어 있으므로 false로 설정
+  },
+});
+
+export default i18n;
